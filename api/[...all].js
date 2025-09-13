f1623b258a7c72a9919a78531e2290b3693f74e8
TimmyJun <timmy40512@yahoo.com>
2025-09-13 20:04:45 +0800

    路由修正

--- api/[...all].js
+++ api/[...all].js
@@ -1,10 +1,8 @@
 const app = require('../server/app')
 
 module.exports = (req, res) => {
-  console.log('[api/index] incoming:', req.method, req.url);
   if (!req.url.startsWith('/api')) {
     req.url = '/api' + req.url;
-    console.log('[api/index] rewrote to:', req.url);
   }
   return app(req, res);
 };
