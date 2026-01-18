7ddd057f521eb92566faff70e76cc5c88c3a8435
TimmyJun <timmy40512@yahoo.com>
2026-01-18 18:24:13 +0800

    商品上稿

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -1,53 +1,69 @@
 const productDefs = [
   {
-    slug: "double-up-plaid-shirt",
-    title: "Double Up Plaid Shirt",
-    price: 3280,
+    slug: "break-down-sweatshirt",
+    title: "Break Down Sweatshirt",
+    price: 2080,
     year: 2025,
     season: "AW",
     description: `* Boxy fit<br>
-* Double Layered also can be torn by your own<br>
+* Destroyed and faded detail everywhere<br>
 * Metal Logo Tag<br>
-* Bottle cap button<br>
-* Destroyed at the bottom and sleeves<br>
 * 100% Cotton<br>
 <br>
 * 短寬版型<br>
-* 雙層格紋面料，可自行撕毀破壞<br>
+* 整體破壞水洗做舊<br>
 * 金屬 Logo<br>
-* 破壞毛絮處理於下擺與袖口<br>
 * 100% 棉材質<br>
 <br>
 * Size 1<br>
-胸圍 Chest - 60cm<br>
-衣長 Length - 56cm<br>
-袖長 Sleeve - 76cm<br>
+胸圍 Chest - 52cm<br>
+衣長 Length - 50cm<br>
+袖長 Sleeve - 70cm<br>
 <br>
 * Size 2<br>
-胸圍 Chest - 62cm<br>
-衣長 Length - 62cm<br>
-袖長 Sleeve - 78cm<br>
+胸圍 Chest - 55cm<br>
+衣長 Length - 55cm<br>
+袖長 Sleeve - 73cm<br>
 <br>
-* 尺寸皆為商品平量數據，單位皆為公分<br>
-* Measurements are all in centimeters`,
+* Size 3<br>
+胸圍 Chest - 58cm<br>
+衣長 Length - 60cm<br>
+袖長 Sleeve - 76cm<br>
+<br>
+* 尺寸皆為商品平量數據，單位皆為公分 *<br>
+* Measurements are all in centimeters *
+`,
     variants: [
       {
-        color: "Yellow",
-        colorCode: "#EDC247",
+        color: "Faded Black",
+        colorCode: "#111111",
         imageFiles: [
-          "yellow-1.jpg",
-          "yellow-2.jpg",
-          "yellow-3.jpg",
-          "yellow-4.jpg",
-          "yellow-5.jpg",
-          "yellow-6.jpg",
-          "yellow-7.jpg",
-          "yellow-8.jpg",
-          "yellow-9.jpg",
+          "black-1.jpg",
+          "black-2.jpg",
+          "black-3.jpg",
+          "black-4.jpg",
+          "black-5.jpg",
         ],
         sizes: [
           { label: "1", stock: 9 },
           { label: "2", stock: 9 },
+          { label: "3", stock: 9 }
+        ]
+      },
+      {
+        color: "Faded Blue",
+        colorCode: "#090C4F",
+        imageFiles: [
+          "blue-1.jpg",
+          "blue-2.jpg",
+          "blue-3.jpg",
+          "blue-4.jpg",
+          "blue-5.jpg",
+        ],
+        sizes: [
+          { label: "1", stock: 9 },
+          { label: "2", stock: 9 },
+          { label: "3", stock: 9 }
         ]
       }
     ]
