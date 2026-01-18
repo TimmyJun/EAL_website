e5fd4bb2741e5dfe5dca4666b08d33aeb86f7761
TimmyJun <timmy40512@yahoo.com>
2026-01-18 15:25:01 +0800

    內容上稿

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -1,28 +1,70 @@
 const productDefs = [
   {
-    slug: "logo-man-short-cap",
-    title: "ETREALOUEST LOGO-MAN SHORT CAP",
-    price: 1380,
+    slug: "removable-hood-zipperjacket",
+    title: "Removable Hood Zipper Jacket",
+    price: 2980,
     year: 2025,
-    season: "SS",
-    description: `* Logo Man embroidery on the brim <br>
-* Handwritten font embroidery on the back <br>
-* Adjustable size <br>
-* Damage effect<br>
+    season: "AW",
+    description: `* Boxy fit <br>
+* Removable Hood also can be scarf <br>
+* Metal Logo Tag <br>
+* 100% Cotton<br>
 <br>
-* Logo Man刺繡在帽簷處<br>
-* 手寫字體在背後<br>
-* 可調節大小<br>
-* 破壞效果`,
+* 短寬版型<br>
+* 可拆卸帽子 也可以單獨作為帽子<br>
+* 金屬Logo<br>
+* 100% 棉材質<br>
+<br>
+* Size 1<br>
+胸圍 Chest - 60cm<br>
+衣長 Length - 50cm<br>
+袖長 Sleeve -70cm<br>
+<br>
+* Size 2<br>
+胸圍 Chest - 63cm<br>
+衣長 Length - 55cm<br>
+袖長 Sleeve -73cm<br>
+<br>
+* Size 3<br>
+胸圍 Chest - 66cm<br>
+衣長 Length - 60cm<br>
+袖長 Sleeve -76cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
     variants: [
       {
-        color: "Black",
+        color: "Black Red",
         colorCode: "#111111",
         imageFiles: [
-          "cap-photo-7.png"
+          "jacket-black-1.jpg",
+          "jacket-black-2.jpg",
+          "jacket-black-3.jpg",
+          "jacket-black-4.jpg",
+          "jacket-black-5.jpg",
+          "jacket-black-6.jpg"
         ],
         sizes: [
-          { label: "1", stock: 9 }
+          { label: "1", stock: 9 },
+          { label: "2", stock: 9 },
+          { label: "3", stock: 9 }
+        ]
+      },
+      {
+        color: "Grey Blue",
+        colorCode: "#e5e5e5",
+        imageFiles: [
+          "jacket-white-1.jpg",
+          "jacket-white-2.jpg",
+          "jacket-white-3.jpg",
+          "jacket-white-4.jpg",
+          "jacket-white-5.jpg",
+          "jacket-white-6.jpg"
+        ],
+        sizes: [
+          { label: "1", stock: 9 },
+          { label: "2", stock: 9 },
+          { label: "3", stock: 9 }
         ]
       }
     ]
