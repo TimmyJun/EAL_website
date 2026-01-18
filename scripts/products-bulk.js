035c42637156d5aa6be35e0ea1cca795f242b385
TimmyJun <timmy40512@yahoo.com>
2026-01-18 18:11:20 +0800

    商品上稿

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -1,70 +1,53 @@
 const productDefs = [
   {
-    slug: "removable-hood-zipperjacket",
-    title: "Removable Hood Zipper Jacket",
-    price: 2980,
+    slug: "double-up-plaid-shirt",
+    title: "Double Up Plaid Shirt",
+    price: 3280,
     year: 2025,
     season: "AW",
-    description: `* Boxy fit <br>
-* Removable Hood also can be scarf <br>
-* Metal Logo Tag <br>
+    description: `* Boxy fit<br>
+* Double Layered also can be torn by your own<br>
+* Metal Logo Tag<br>
+* Bottle cap button<br>
+* Destroyed at the bottom and sleeves<br>
 * 100% Cotton<br>
 <br>
 * 短寬版型<br>
-* 可拆卸帽子 也可以單獨作為帽子<br>
-* 金屬Logo<br>
+* 雙層格紋面料，可自行撕毀破壞<br>
+* 金屬 Logo<br>
+* 破壞毛絮處理於下擺與袖口<br>
 * 100% 棉材質<br>
 <br>
 * Size 1<br>
 胸圍 Chest - 60cm<br>
-衣長 Length - 50cm<br>
-袖長 Sleeve -70cm<br>
+衣長 Length - 56cm<br>
+袖長 Sleeve - 76cm<br>
 <br>
 * Size 2<br>
-胸圍 Chest - 63cm<br>
-衣長 Length - 55cm<br>
-袖長 Sleeve -73cm<br>
+胸圍 Chest - 62cm<br>
+衣長 Length - 62cm<br>
+袖長 Sleeve - 78cm<br>
 <br>
-* Size 3<br>
-胸圍 Chest - 66cm<br>
-衣長 Length - 60cm<br>
-袖長 Sleeve -76cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
+* 尺寸皆為商品平量數據，單位皆為公分<br>
+* Measurements are all in centimeters`,
     variants: [
       {
-        color: "Black Red",
-        colorCode: "#111111",
+        color: "Yellow",
+        colorCode: "#EDC247",
         imageFiles: [
-          "jacket-black-1.jpg",
-          "jacket-black-2.jpg",
-          "jacket-black-3.jpg",
-          "jacket-black-4.jpg",
-          "jacket-black-5.jpg",
-          "jacket-black-6.jpg"
+          "yellow-1.jpg",
+          "yellow-2.jpg",
+          "yellow-3.jpg",
+          "yellow-4.jpg",
+          "yellow-5.jpg",
+          "yellow-6.jpg",
+          "yellow-7.jpg",
+          "yellow-8.jpg",
+          "yellow-9.jpg",
         ],
         sizes: [
           { label: "1", stock: 9 },
           { label: "2", stock: 9 },
-          { label: "3", stock: 9 }
-        ]
-      },
-      {
-        color: "Grey Blue",
-        colorCode: "#e5e5e5",
-        imageFiles: [
-          "jacket-white-1.jpg",
-          "jacket-white-2.jpg",
-          "jacket-white-3.jpg",
-          "jacket-white-4.jpg",
-          "jacket-white-5.jpg",
-          "jacket-white-6.jpg"
-        ],
-        sizes: [
-          { label: "1", stock: 9 },
-          { label: "2", stock: 9 },
-          { label: "3", stock: 9 }
         ]
       }
     ]
@@ -73,4 +56,4 @@ const productDefs = [
   // 之後要再加其他商品就繼續往下加
 ];
 
-module.exports = { productDefs };
+module.exports = { productDefs }
