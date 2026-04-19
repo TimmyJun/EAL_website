a66b66beedc7adb2ed351030a99bf89164bdbffc
TimmyJun <timmy40512@yahoo.com>
2026-04-19 14:41:25 +0800

    2023SS All

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -1,16 +1,16 @@
 const productDefs = [
   {
-    "slug": "Mess-Words-Tee",
-    "title": "Mess Words Tee",
-    "price": 1180,
+    "slug": "Round-Neck-Tee ",
+    "title": "Round Neck Tee",
+    "price": 1280,
     "year": 2023,
     "season": "SS",
     "description": `
-   * Handwritten font Logo words <br>
+   * Collars in different colors <br>
 * Loose Fit <br>
 * 100% Cotton<br>
 <br>
-* 草寫文字字體<br>
+* 領子撞色設計<br>
 * 寬鬆版型<br>
 * 100% 棉材質<br>
 <br>
@@ -36,14 +36,14 @@ const productDefs = [
         "color": "white",
         "colorCode": "#FFFFFF",
         "imageFiles": [
-          "2023SS白色草寫短袖-01.jpg",
-          "2023SS白色草寫短袖-02.jpg",
-          "2023SS白色草寫短袖-03.jpg",
-          "2023SS白色草寫短袖-04.jpg"
+          "2023SS黑色領子短袖-01.jpg",
+          "2023SS黑色領子短袖-02.jpg",
+          "2023SS黑色領子短袖-03.jpg",
+          "2023SS黑色領子短袖-04.jpg"
         ],
         sizes: [
           { label: "1", stock: 0 },
-          { label: "2", stock: 0 },
+          { label: "2", stock: 3 },
           { label: "3", stock: 0 }
         ]
       },
@@ -51,13 +51,13 @@ const productDefs = [
         "color": "black",
         "colorCode": "#000000",
         "imageFiles": [
-          "2023SS黑色草寫短袖-01.jpg",
-          "2023SS黑色草寫短袖-02.jpg",
-          "2023SS黑色草寫短袖-03.jpg",
-          "2023SS黑色草寫短袖-04.jpg"
+          "2023SS白色領子短袖-01.jpg",
+          "2023SS白色領子短袖-02.jpg",
+          "2023SS白色領子短袖-03.jpg",
+          "2023SS白色領子短袖-04.jpg"
         ],
         sizes: [
-          { label: "1", stock: 0 },
+          { label: "1", stock: 1 },
           { label: "2", stock: 0 },
           { label: "3", stock: 0 }
         ]
@@ -65,53 +65,166 @@ const productDefs = [
     ]
   },
   {
-    "slug": "Straight-Pants",
-    "title": "Straight Pants",
-    "price": 1880,
+    "slug": "Classic-EAL-Tee",
+    "title": "Classic EAL Tee",
+    "price": 1180,
     "year": 2023,
     "season": "SS",
     "description": `
-  * EAL logo embroidery <br>
-* Straight-leg fit <br>
+  * EAL classic fonts design. <br>
+* Loose Fit <br>
 * 100% Cotton<br>
 <br>
-* EAL Logo刺繡<br>
-* 直筒褲版型<br>
+* 經典Logo設計<br>
+* 寬鬆版型<br>
 * 100% 棉材質<br>
 <br>
 * Size 1<br>
-腰圍 Waist - 73cm<br>
-褲長 Length - 103cm<br>
-臀圍 Hipline - 96cm<br>
+胸寬 Chest - 60cm<br>
+衣長 Length - 68cm<br>
+肩寬 Shoulder - 57cm<br>
 <br>
 * Size 2<br>
-腰圍 Waist - 80cm<br>
-褲長 Length - 106cm<br>
-臀圍 Hipline - 104cm<br>
+胸寬 Chest - 63cm<br>
+衣長 Length - 71cm<br>
+肩寬 Shoulder - 59cm<br>
 <br>
 * Size 3<br>
-腰圍 Waist - 85cm<br>
-褲長 Length - 109cm<br>
-臀圍 Hipline - 112cm<br>
+胸寬 Chest - 66cm<br>
+衣長 Length - 74cm<br>
+肩寬 Shoulder - 61cm<br>
 <br>
 * 尺寸皆為商品平量數據 單位皆為公分 *<br>
 * Measurements are all in centimeters *`,
     "variants": [
       {
-        "color": "cream",
-        "colorCode": "#FFFDD0",
+        "color": "black",
+        "colorCode": "#000000",
         "imageFiles": [
-          "2023SS米色喇叭褲-01.jpg",
-          "2023SS米色喇叭褲-02.jpg",
-          "2023SS米色喇叭褲-03.jpg",
-          "2023SS米色喇叭褲-04.jpg",
-          "2023SS米色喇叭褲-05.jpg",
-          "2023SS米色喇叭褲-06.jpg",
+          "2023SS黑色EAL短袖-01.jpg",
+          "2023SS黑色EAL短袖-02.jpg",
+          "2023SS黑色EAL短袖-03.jpg",
+          "2023SS黑色EAL短袖-04.jpg",
+          "2023SS黑色EAL短袖-05.jpg",
         ],
         sizes: [
-          { label: "1", stock: 0 },
-          { label: "2", stock: 5 },
-          { label: "3", stock: 13 }
+          { label: "1", stock: 15 },
+          { label: "2", stock: 10 },
+          { label: "3", stock: 2 }
+        ]
+      },
+      {
+        "color": "white",
+        "colorCode": "#FFFFFF",
+        "imageFiles": [
+          "2023SS白色EAL短袖-01.jpg",
+          "2023SS白色EAL短袖-02.jpg",
+          "2023SS白色EAL短袖-03.jpg",
+          "2023SS白色EAL短袖-04.jpg",
+        ],
+        sizes: [
+          { label: "1", stock: 13 },
+          { label: "2", stock: 9 },
+          { label: "3", stock: 2 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "White-Line-Tee",
+    "title": "White Line Tee",
+    "price": 1280,
+    "year": 2023,
+    "season": "SS",
+    "description": `
+  * White Line at the front <br>
+* Loose Fit <br>
+* 100% Cotton<br>
+<br>
+* 白色貫穿線在胸前<br>
+* 寬鬆版型<br>
+* 100% 棉材質<br>
+<br>
+* Size 1<br>
+胸寬 Chest - 60cm<br>
+衣長 Length - 68cm<br>
+肩寬 Shoulder - 57cm<br>
+<br>
+* Size 2<br>
+胸寬 Chest - 63cm<br>
+衣長 Length - 71cm<br>
+肩寬 Shoulder - 59cm<br>
+<br>
+* Size 3<br>
+胸寬 Chest - 66cm<br>
+衣長 Length - 74cm<br>
+肩寬 Shoulder - 61cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "grey",
+        "colorCode": "#808080",
+        "imageFiles": [
+          "2023SS灰色line短袖-01.jpg",
+          "2023SS灰色line短袖-02.jpg",
+          "2023SS灰色line短袖-03.jpg",
+          "2023SS灰色line短袖-04.jpg",
+          "2023SS灰色line短袖-05.jpg",
+        ],
+        sizes: [
+          { label: "1", stock: 2 },
+          { label: "2", stock: 0 },
+          { label: "3", stock: 0 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "Logo-Knit-Vest",
+    "title": "Logo Knit Vest",
+    "price": 1680,
+    "year": 2023,
+    "season": "SS",
+    "description": `
+  * Slightly Fit <br>
+* Classic logo design with logo pattern on the back <br>
+* Round neckline with adjustable metal buttons <br>
+* Using the YKK zipper <br>
+* Two inside pockets<br>
+<br>
+* 白色貫穿線在胸前<br>
+* 寬鬆版型<br>
+* 100% 棉材質<br>
+<br>
+* Size 1<br>
+身長 Length - 70cm<br>
+胸寬 Chest - 59cm<br>
+肩寬 Shoulder - 44cm<br>
+<br>
+* Size 2<br>
+身長 Length - 75cm<br>
+胸寬 Chest - 64cm<br>
+肩寬 Shoulder - 49cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "yellow",
+        "colorCode": "#e2bc3e",
+        "imageFiles": [
+          "2023SS黃色拉鍊背心-01.jpg",
+          "2023SS黃色拉鍊背心-02.jpg",
+          "2023SS黃色拉鍊背心-03.jpg",
+          "2023SS黃色拉鍊背心-04.jpg",
+          "2023SS黃色拉鍊背心-05.jpg",
+          "2023SS黃色拉鍊背心-06.jpg",
+        ],
+        sizes: [
+          { label: "1", stock: 5 },
+          { label: "2", stock: 13 }
         ]
       }
     ]
