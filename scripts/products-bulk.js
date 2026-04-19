295e92ddb73d9b411edc33d10f1a81689e57c908
TimmyJun <timmy40512@yahoo.com>
2026-04-19 15:38:47 +0800

    2025SS

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -1,131 +1,352 @@
 const productDefs = [
   {
-    "slug": "Logo-Vintage-Destroyed-Tee",
-    "title": "Logo Vintage Destroyed Tee",
+    "slug": "Layere-Cotton-Shorts",
+    "title": "Layered Cotton Shorts",
     "price": 1480,
-    "year": 2024,
+    "year": 2025,
     "season": "SS",
     "description": `
-  * Crop and Slim fit <br>
-* Destroyed design on both front and back <br>
-* Large surface of DTF printing on the front <br>
-* 100% Cotton and Linen Fabrics<br>
+  * Short fit <br>
+* Embroidered logo<br>
+<br>
+* 膝蓋上版型<br>
+* Logo刺繡<br>
 <br>
 * Size 1<br>
-胸圍 Chest - 59cm<br>
-衣長 Length - 63cm<br>
-袖長 Sleeve - 36cm<br>
+腰圍 Waist - 72cm<br>
+褲長 Length - 40cm<br>
+大腿圍 Thighs - 64cm<br>
 <br>
 * Size 2<br>
-胸圍 Chest - 61cm<br>
-衣長 Length - 66cm<br>
-袖長 Sleeve - 37cm<br>
+腰圍 Waist - 75cm<br>
+褲長 Length - 44cm<br>
+大腿圍 Thighs - 68cm<br>
 <br>
 * Size 3<br>
-胸圍 Chest - 63cm<br>
-衣長 Length - 69cm<br>
-袖長 Sleeve - 38cm<br>
+腰圍 Waist - 78cm<br>
+褲長 Length - 48cm<br>
+大腿圍 Thighs - 72cm<br>
 <br>
 * 尺寸皆為商品平量數據 單位皆為公分 *<br>
 * Measurements are all in centimeters *`,
     "variants": [
       {
-        "color": "Vintage Black",
-        "colorCode": "#888888",
+        "color": "Olive Green",
+        "colorCode": "#5f7754",
         "imageFiles": [
-          "2024SS黑色水洗logo短袖-01.jpg",
-          "2024SS黑色水洗logo短袖-02.jpg",
-          "2024SS黑色水洗logo短袖-03.jpg",
-          "2024SS黑色水洗logo短袖-04.jpg",
-          "2024SS黑色水洗logo短袖-05.jpg",
-          "2024SS黑色水洗logo短袖-06.jpg"
+          "2025SS綠色活頁棉短褲-01.jpg",
+          "2025SS綠色活頁棉短褲-02.jpg",
+          "2025SS綠色活頁棉短褲-03.jpg",
+          "2025SS綠色活頁棉短褲-04.jpg",
+          "2025SS綠色活頁棉短褲-05.jpg",
+          "2025SS綠色活頁棉短褲-06.jpg"
         ],
         sizes: [
-          { label: "1", stock: 1 },
-          { label: "2", stock: 10 },
-          { label: "3", stock: 6 }
-        ]
-      },
-      {
-        "color": "Vintage White",
-        "colorCode": "#FFFFFF",
-        "imageFiles": [
-          "2024SS白色水洗logo短袖-01.jpg",
-          "2024SS白色水洗logo短袖-02.jpg",
-          "2024SS白色水洗logo短袖-03.jpg",
-          "2024SS白色水洗logo短袖-04.jpg",
-          "2024SS白色水洗logo短袖-05.jpg",
-          "2024SS白色水洗logo短袖-06.jpg"
-        ],
-        sizes: [
-          { label: "1", stock: 2 },
-          { label: "2", stock: 27 },
-          { label: "3", stock: 20 }
+          { label: "1", stock: 5 },
+          { label: "2", stock: 17 },
+          { label: "3", stock: 8 }
         ]
       }
     ]
   },
   {
-    "slug": "Dept-Vintage-Destroyed-Tee",
-    "title": "Dept Vintage Destroyed Tee",
-    "price": 1480,
-    "year": 2024,
+    "slug": "Tight-Fit-Tee",
+    "title": "Tight Fit Tee",
+    "price": 1280,
+    "year": 2025,
     "season": "SS",
     "description": `
-  * Crop and Slim fit <br>
-* Destroyed design on both front and back <br>
-* Large surface of DTF printing on both side<br>
+  * Tight fit <br>
+* Logo metal tag <br>
+* 100% Cotton<br>
+<br>
+* 合身正肩的版型<br>
+* Logo金屬標<br>
+* 100% 棉材質<br>
 <br>
 * Size 1<br>
-胸圍 Chest - 59cm<br>
-衣長 Length - 63cm<br>
+胸圍 Chest - 100m<br>
+衣長 Length - 64cm<br>
+肩寬 Shoulder - 41cm<br>
+袖長 Sleeve - 18cm<br>
+<br>
+* Size 2<br>
+胸圍 Chest - 104cm<br>
+衣長 Length - 66cm<br>
+肩寬 Shoulder - 42cm<br>
+袖長 Sleeve - 18cm<br>
+<br>
+* Size 3<br>
+胸圍 Chest - 110cm<br>
+衣長 Length - 68cm<br>
+肩寬 Shoulder - 44cm<br>
+袖長 Sleeve - 20cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "Black",
+        "colorCode": "#000000",
+        "imageFiles": [
+          "2025SS黑色正肩短袖-01.png",
+          "2025SS黑色正肩短袖-02.png",
+          "2025SS黑色正肩短袖-03.png"
+        ],
+        sizes: [
+          { label: "1", stock: 0 },
+          { label: "2", stock: 0 },
+          { label: "3", stock: 0 }
+        ]
+      },
+      {
+        "color": "White",
+        "colorCode": "#FFFFFF",
+        "imageFiles": [
+          "2025SS白色正肩短袖-01.png",
+          "2025SS白色正肩短袖-02.png",
+          "2025SS白色正肩短袖-03.png"
+        ],
+        sizes: [
+          { label: "1", stock: 1 },
+          { label: "2", stock: 0 },
+          { label: "3", stock: 0 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "Below-Knee-Pants",
+    "title": "Below Knee Pants",
+    "price": 1680,
+    "year": 2025,
+    "season": "SS",
+    "description": `
+  * Loose fit <br>
+* Logo leather tag<br>
+<br>
+* 寬鬆膝下版型<br>
+* 五金皮牌<br>
+<br>
+* Size 1<br>
+腰圍 Waist - 70cm<br>
+褲長 Length - 60cm<br>
+<br>
+* Size 2<br>
+腰圍 Waist - 74cm<br>
+褲長 Length - 62cm<br>
+<br>
+* Size 3<br>
+腰圍 Waist - 78cm<br>
+褲長 Length - 64cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "Black",
+        "colorCode": "#000000",
+        "imageFiles": [
+          "2025SS黑色水洗七分褲-01.png",
+          "2025SS黑色水洗七分褲-02.png",
+          "2025SS黑色水洗七分褲-03.png",
+          "2025SS黑色水洗七分褲-04.png",
+          "2025SS黑色水洗七分褲-05.png",
+          "2025SS黑色水洗七分褲-06.png",
+          "2025SS黑色水洗七分褲-07.png",
+        ],
+        sizes: [
+          { label: "1", stock: 3 },
+          { label: "2", stock: 27 },
+          { label: "3", stock: 1 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "Layered-Mesh-Boxy-Tee",
+    "title": "Layered Mesh Boxy Tee",
+    "price": 1480,
+    "year": 2025,
+    "season": "SS",
+    "description": `
+  * Boxy fit <br>
+* Logo metal tag <br>
+* Combined with mesh material <br>
+* 100% Cotton<br>
+<br>
+* 短寬版型<br>
+* Logo金屬標<br>
+* 混入網眼材質拼接<br>
+* 100% 棉材質<br>
+<br>
+* Size 1<br>
+胸圍 Chest - 116m<br>
+衣長 Length - 58cm<br>
 袖長 Sleeve - 36cm<br>
 <br>
 * Size 2<br>
-胸圍 Chest - 61cm<br>
-衣長 Length - 66cm<br>
+胸圍 Chest - 120cm<br>
+衣長 Length - 60cm<br>
 袖長 Sleeve - 37cm<br>
 <br>
 * Size 3<br>
-胸圍 Chest - 63cm<br>
-衣長 Length - 69cm<br>
+胸圍 Chest - 124cm<br>
+衣長 Length - 62cm<br>
 袖長 Sleeve - 38cm<br>
 <br>
 * 尺寸皆為商品平量數據 單位皆為公分 *<br>
 * Measurements are all in centimeters *`,
     "variants": [
       {
-        "color": "Vintage Black",
-        "colorCode": "#888888",
+        "color": "Black",
+        "colorCode": "#000000",
         "imageFiles": [
-          "2024SS黑色水洗dept短袖-01.jpg",
-          "2024SS黑色水洗dept短袖-02.jpg",
-          "2024SS黑色水洗dept短袖-03.jpg",
-          "2024SS黑色水洗dept短袖-04.jpg",
-          "2024SS黑色水洗dept短袖-05.jpg",
-          "2024SS黑色水洗dept短袖-06.jpg",
+          "2025SS黑色網眼短袖-01.png",
+          "2025SS黑色網眼短袖-02.png",
+          "2025SS黑色網眼短袖-03.png",
+          "2025SS黑色網眼短袖-04.png",
+          "2025SS黑色網眼短袖-05.png"
         ],
         sizes: [
           { label: "1", stock: 0 },
           { label: "2", stock: 0 },
-          { label: "3", stock: 11 }
+          { label: "3", stock: 0 }
         ]
       },
       {
-        "color": "Vintage White",
+        "color": "White",
         "colorCode": "#FFFFFF",
         "imageFiles": [
-          "2024SS白色水洗dept短袖-01.jpg",
-          "2024SS白色水洗dept短袖-02.jpg",
-          "2024SS白色水洗dept短袖-03.jpg",
-          "2024SS白色水洗dept短袖-04.jpg",
-          "2024SS白色水洗dept短袖-05.jpg",
-          "2024SS白色水洗dept短袖-06.jpg",
+          "2025SS白色網眼短袖-01.png",
+          "2025SS白色網眼短袖-02.png",
+          "2025SS白色網眼短袖-03.png",
+          "2025SS白色網眼短袖-04.png",
+          "2025SS白色網眼短袖-05.png"
         ],
         sizes: [
+          { label: "1", stock: 2 },
+          { label: "2", stock: 0 },
+          { label: "3", stock: 2 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "Reversed-Baggy-Double-Knees-Jeans",
+    "title": "Reversed Baggy Double Knees Jeans",
+    "price": 3280,
+    "year": 2025,
+    "season": "SS",
+    "description": `
+  * Baggy Loose fit <br>
+* Logo leather tag <br>
+* Reversed pocket <br>
+* Double Knees <br>
+* Destroy design<br>
+<br>
+* 寬鬆寬褲版型<br>
+* 五金皮牌<br>
+* 反面口袋<br>
+* 雙膝蓋<br>
+* 破壞做舊<br>
+<br>
+* Size 0（WMS 女碼）<br>
+腰圍 Waist - 70cm<br>
+大腿 Thigh - 35cm<br>
+褲長 Length - 102cm<br>
+<br>
+* Size 1<br>
+腰圍 Waist - 78cm<br>
+大腿 Thigh - 36cm<br>
+褲長 Length - 106cm<br>
+<br>
+* Size 2<br>
+腰圍 Waist - 82cm<br>
+大腿 Thigh - 37cm<br>
+褲長 Length - 108cm<br>
+<br>
+* Size 3<br>
+腰圍 Waist - 86cm<br>
+大腿 Thigh - 38cm<br>
+褲長 Length - 110cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "Washed Blue",
+        "colorCode": "#9cadd9",
+        "imageFiles": [
+          "2025SS藍色牛仔褲-01.png",
+          "2025SS藍色牛仔褲-02.png",
+          "2025SS藍色牛仔褲-03.png",
+          "2025SS藍色牛仔褲-04.png",
+          "2025SS藍色牛仔褲-05.png",
+          "2025SS藍色牛仔褲-06.png",
+          "2025SS藍色牛仔褲-07.png",
+          "2025SS藍色牛仔褲-08.png"
+        ],
+        sizes: [
+          { label: "0", stock: 0 },
           { label: "1", stock: 0 },
-          { label: "2", stock: 17 },
-          { label: "3", stock: 28 }
+          { label: "2", stock: 0 },
+          { label: "3", stock: 0 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "Double-Layered-Nylon-Cargo-Baggy-Pants",
+    "title": "Double Layered Nylon Cargo Baggy Pants",
+    "price": 2780,
+    "year": 2025,
+    "season": "SS",
+    "description": `
+  * Baggy fit <br>
+* Embroidered logo <br>
+* Two-tone color<br>
+<br>
+* 寬鬆版型<br>
+* Logo刺繡<br>
+* 兩種顏色拼接<br>
+<br>
+* Size 0<br>
+腰圍 Waist - 60cm<br>
+褲長 Length - 102cm<br>
+<br>
+* Size 1<br>
+腰圍 Waist - 70cm<br>
+褲長 Length - 104cm<br>
+<br>
+* Size 2<br>
+腰圍 Waist - 75cm<br>
+褲長 Length - 106cm<br>
+<br>
+* Size 3<br>
+腰圍 Waist - 80cm<br>
+褲長 Length - 108cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "Black Green",
+        "colorCode": "#4c5345",
+        "imageFiles": [
+          "2025SS雙腰頭尼龍褲-01.jpg",
+          "2025SS雙腰頭尼龍褲-02.jpg",
+          "2025SS雙腰頭尼龍褲-03.jpg",
+          "2025SS雙腰頭尼龍褲-04.jpg",
+          "2025SS雙腰頭尼龍褲-05.jpg",
+          "2025SS雙腰頭尼龍褲-06.jpg",
+          "2025SS雙腰頭尼龍褲-07.jpg"
+        ],
+        sizes: [
+          { label: "0", stock: 11 },
+          { label: "1", stock: 8 },
+          { label: "2", stock: 10 },
+          { label: "3", stock: 0 }
         ]
       }
     ]
