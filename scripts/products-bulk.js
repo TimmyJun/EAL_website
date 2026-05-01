d6ae1e985bf5f714bf3392f9d706fb424e2d71fc
TimmyJun <timmy40512@yahoo.com>
2026-05-01 16:02:43 +0800

    add new product

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -1,473 +1,64 @@
 const productDefs = [
   {
-    "slug": "Henley-Waffle-Long-Sleeve",
-    "title": "Henley Waffle Long Sleeve",
-    "price": 1880,
+    "slug": "Reversed-Baggy-Double-Knees-Jeans",
+    "title": "Reversed Baggy Double Knees Jeans",
+    "price": 3280,
     "year": 2025,
-    "season": "FW",
+    "season": "SS",
     "description": `
-  * Boxy fit <br>
-* Henley Sleeve <br>
-* Vintage buttons on the sleeve and collar <br>
-* Metal Logo Tag <br>
-* 100% Cotton<br>
+  * Baggy Loose fit <br>
+* Logo leather tag <br>
+* Reversed pocket <br>
+* Double Knees <br>
+* Destroy design<br>
 <br>
-* 短寬版型<br>
-* 亨利領華夫格<br>
-* 做舊鈕釦在衣領和手袖<br>
-* 金屬Logo<br>
-* 100% 棉材質<br>
+* 寬鬆寬褲版型<br>
+* 五金皮牌<br>
+* 反面口袋<br>
+* 雙膝蓋<br>
+* 破壞做舊<br>
+<br>
+* Size 0（WMS 女碼）<br>
+腰圍 Waist - 70cm<br>
+大腿 Thigh - 35cm<br>
+褲長 Length - 102cm<br>
 <br>
 * Size 1<br>
-胸圍 Chest - 62cm<br>
-衣長 Length - 58cm<br>
-袖長 Sleeve - 57cm<br>
+腰圍 Waist - 78cm<br>
+大腿 Thigh - 36cm<br>
+褲長 Length - 106cm<br>
 <br>
 * Size 2<br>
-胸圍 Chest - 64cm<br>
-衣長 Length - 60cm<br>
-袖長 Sleeve - 58cm<br>
+腰圍 Waist - 82cm<br>
+大腿 Thigh - 37cm<br>
+褲長 Length - 108cm<br>
 <br>
 * Size 3<br>
-胸圍 Chest - 66cm<br>
-衣長 Length - 62cm<br>
-袖長 Sleeve - 59cm<br>
+腰圍 Waist - 86cm<br>
+大腿 Thigh - 38cm<br>
+褲長 Length - 110cm<br>
 <br>
 * 尺寸皆為商品平量數據 單位皆為公分 *<br>
 * Measurements are all in centimeters *`,
     "variants": [
       {
-        "color": "Black",
-        "colorCode": "#000000",
+        "color": "Washed Blue",
+        "colorCode": "#2f4a80",
         "imageFiles": [
-          "2025FW黑色華夫格-01.png",
-          "2025FW黑色華夫格-02.png",
-          "2025FW黑色華夫格-03.png",
-          "2025FW黑色華夫格-04.png"
+          "2025SS藍色牛仔褲-01.png",
+          "2025SS藍色牛仔褲-02.png",
+          "2025SS藍色牛仔褲-03.png",
+          "2025SS藍色牛仔褲-04.png",
+          "2025SS藍色牛仔褲-05.png",
+          "2025SS藍色牛仔褲-06.png",
+          "2025SS藍色牛仔褲-07.png",
+          "2025SS藍色牛仔褲-08.png"
         ],
         sizes: [
-          { label: "1", stock: 0 },
-          { label: "2", stock: 0 },
-          { label: "3", stock: 0 }
-        ]
-      },
-      {
-        "color": "Grey",
-        "colorCode": "#808080",
-        "imageFiles": [
-          "2025FW灰色華夫格-01.png",
-          "2025FW灰色華夫格-02.png",
-          "2025FW灰色華夫格-03.png",
-          "2025FW灰色華夫格-04.png"
-        ],
-        sizes: [
-          { label: "1", stock: 0 },
-          { label: "2", stock: 6 },
-          { label: "3", stock: 0 }
-        ]
-      },
-      {
-        "color": "Ash Brown",
-        "colorCode": "#ffffff",
-        "imageFiles": [
-          "2025FW亞麻色華夫格-01.png",
-          "2025FW亞麻色華夫格-02.png",
-          "2025FW亞麻色華夫格-03.png",
-          "2025FW亞麻色華夫格-04.png"
-        ],
-        sizes: [
-          { label: "1", stock: 5 },
-          { label: "2", stock: 1 },
-          { label: "3", stock: 3 }
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "Stright-Long-Sleeve",
-    "title": "Stright Long Sleeve",
-    "price": 1980,
-    "year": 2025,
-    "season": "FW",
-    "description": `
-  * Boxy fit <br>
-* Padded shoulder <br>
-* Metal Logo Tag <br>
-* 100% Cotton<br>
-<br>
-* 短寬版型<br>
-* 肩膀處增加墊片 凸顯肩膀效果<br>
-* 金屬Logo<br>
-* 100% 棉材質<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 58cm<br>
-衣長 Length - 60cm<br>
-袖長 Sleeve - 58cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 60cm<br>
-衣長 Length - 62cm<br>
-袖長 Sleeve - 59cm<br>
-<br>
-* Size 3<br>
-胸圍 Chest - 62cm<br>
-衣長 Length - 64cm<br>
-袖長 Sleeve - 60cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Black",
-        "colorCode": "#000000",
-        "imageFiles": [
-          "2025FW黑色正肩長袖-01.png",
-          "2025FW黑色正肩長袖-02.png",
-          "2025FW黑色正肩長袖-03.png",
-          "2025FW黑色正肩長袖-04.png"
-        ],
-        sizes: [
-          { label: "1", stock: 2 },
-          { label: "2", stock: 10 },
-          { label: "3", stock: 0 }
-        ]
-      },
-      {
-        "color": "White",
-        "colorCode": "#FFFFFF",
-        "imageFiles": [
-          "2025FW白色正肩長袖-01.png",
-          "2025FW白色正肩長袖-02.png",
-          "2025FW白色正肩長袖-03.png",
-          "2025FW白色正肩長袖-04.png"
-        ],
-        sizes: [
-          { label: "1", stock: 2 },
-          { label: "2", stock: 16 },
-          { label: "3", stock: 4 }
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "Hole-Knit-Double-Zipper-Sweater",
-    "title": "Hole Knit Double Zipper Sweater",
-    "price": 1880,
-    "year": 2025,
-    "season": "FW",
-    "description": `
-  * Regular fit <br>
-* Double Zipper <br>
-* Hole Knit <br>
-* 100% Cotton<br>
-<br>
-* 常規版型<br>
-* 雙拉鍊<br>
-* 網洞針織<br>
-* 100% 棉材質<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 63cm<br>
-衣長 Length - 54cm<br>
-袖長 Sleeve - 66cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 67cm<br>
-衣長 Length - 60cm<br>
-袖長 Sleeve - 70cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Black",
-        "colorCode": "#000000",
-        "imageFiles": [
-          "2025FW黑色洞洞拉鍊毛衣-01.png",
-          "2025FW黑色洞洞拉鍊毛衣-02.png",
-          "2025FW黑色洞洞拉鍊毛衣-03.png",
-          "2025FW黑色洞洞拉鍊毛衣-04.png",
-          "2025FW黑色洞洞拉鍊毛衣-05.png",
-          "2025FW黑色洞洞拉鍊毛衣-06.png"
-        ],
-        sizes: [
-          { label: "1", stock: 2 },
-          { label: "2", stock: 0 }
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "WMS-Destroyed-Knit-Zipper-Sweater",
-    "title": "WMS Destroyed Knit Zipper Sweater",
-    "price": 1880,
-    "year": 2025,
-    "season": "FW",
-    "description": `
-  * Short fit <br>
-* Destroyed at the bottom and sleeves <br>
-* Metal ring on the hood <br>
-* 100% Cotton<br>
-<br>
-* 短版版型<br>
-* 破壞毛絮處理在下擺跟手袖<br>
-* 金屬扣環在帽子處<br>
-* 100% 棉材質<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 45cm<br>
-衣長 Length - 40cm<br>
-袖長 Sleeve - 70cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 50cm<br>
-衣長 Length - 43cm<br>
-袖長 Sleeve - 73cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Black",
-        "colorCode": "#000000",
-        "imageFiles": [
-          "2025FW黑色破壞拉鍊毛衣-01.png",
-          "2025FW黑色破壞拉鍊毛衣-02.png",
-          "2025FW黑色破壞拉鍊毛衣-03.png",
-          "2025FW黑色破壞拉鍊毛衣-04.png",
-          "2025FW黑色破壞拉鍊毛衣-05.png"
-        ],
-        sizes: [
-          { label: "1", stock: 0 },
-          { label: "2", stock: 0 }
-        ]
-      },
-      {
-        "color": "white",
-        "colorCode": "#FFFFFF",
-        "imageFiles": [
-          "2025FW白色破壞拉鍊毛衣-01.png",
-          "2025FW白色破壞拉鍊毛衣-02.png",
-          "2025FW白色破壞拉鍊毛衣-03.png",
-          "2025FW白色破壞拉鍊毛衣-04.png",
-          "2025FW白色破壞拉鍊毛衣-05.png"
-        ],
-        sizes: [
-          { label: "1", stock: 0 },
-          { label: "2", stock: 0 }
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "Camo-logo-Long-Sleeve",
-    "title": "Camo logo Long Sleeve",
-    "price": 1880,
-    "year": 2025,
-    "season": "FW",
-    "description": `
- * Boxy fit <br>
-* Printed logo <br>
-* Two-Piece Patchwork Long Sleeve<br>
-<br>
-* 短寬版型<br>
-* 印花Logo<br>
-* 假兩件拼接長袖<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 56cm<br>
-衣長 Length - 56cm<br>
-袖長 Sleeve - 65cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 60cm<br>
-衣長 Length - 60cm<br>
-袖長 Sleeve - 70cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Camo Black Green ",
-        "colorCode": "#000000",
-        "imageFiles": [
-          "2025FW迷彩長袖-01.jpg",
-          "2025FW迷彩長袖-02.jpg",
-          "2025FW迷彩長袖-03.jpg",
-          "2025FW迷彩長袖-04.jpg",
-          "2025FW迷彩長袖-05.jpg"
-        ],
-        sizes: [
-          { label: "1", stock: 10 },
-          { label: "2", stock: 0 }
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "Bottle-Cap-Knit-Cardigan",
-    "title": "Bottle Cap Knit Cardigan",
-    "price": 1980,
-    "year": 2025,
-    "season": "FW",
-    "description": `
- * Regular fit <br>
-* Bottle cap button <br>
-* 100% Cotton<br>
-<br>
-* 常規版型<br>
-* 瓶蓋鈕釦<br>
-* 100% 棉材質<br>
-<br>
-* Size 0<br>
-胸圍 Chest - 46cm<br>
-衣長 Length - 50cm<br>
-袖長 Sleeve - 72cm<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 50cm<br>
-衣長 Length - 54cm<br>
-袖長 Sleeve - 74cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 54cm<br>
-衣長 Length - 58cm<br>
-袖長 Sleeve - 76cm<br>
-<br>
-* Size 3<br>
-胸圍 Chest - 58m<br>
-衣長 Length - 62cm<br>
-袖長 Sleeve - 78cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Black",
-        "colorCode": "#000000",
-        "imageFiles": [
-          "2025FW黑色開衫-01.png",
-          "2025FW黑色開衫-02.png",
-          "2025FW黑色開衫-03.png",
-          "2025FW黑色開衫-04.png"
-        ],
-        sizes: [
-          { label: "0", stock: 0 },
-          { label: "1", stock: 0 },
-          { label: "2", stock: 0 },
-          { label: "3", stock: 0 }
-        ]
-      },
-      {
-        "color": "Grey",
-        "colorCode": "#808080",
-        "imageFiles": [
-          "2025FW灰色開衫-01.png",
-          "2025FW灰色開衫-02.png",
-          "2025FW灰色開衫-03.png",
-          "2025FW灰色開衫-04.png"
-        ],
-        sizes: [
-          { label: "0", stock: 0 },
-          { label: "1", stock: 0 },
-          { label: "2", stock: 0 },
-          { label: "3", stock: 0 }
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "Car-Exploded-Long-Sleeve",
-    "title": "Car Exploded Long Sleeve",
-    "price": 1580,
-    "year": 2025,
-    "season": "FW",
-    "description": `
- * Boxy fit <br>
-* Dirty print <br>
-* 100% Cotton<br>
-<br>
-* 短版版型<br>
-* 噴髒做舊處理<br>
-* 100% 棉材質<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 56cm<br>
-衣長 Length - 60cm<br>
-袖長 Sleeve - 70cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 58cm<br>
-衣長 Length - 62cm<br>
-袖長 Sleeve - 72cm<br>
-<br>
-* Size 3<br>
-胸圍 Chest - 60m<br>
-衣長 Length - 64cm<br>
-袖長 Sleeve - 74cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Dirty Khaki",
-        "colorCode": "#b8b7af",
-        "imageFiles": [
-          "2025FW賽車長袖-01.png",
-          "2025FW賽車長袖-02.png",
-          "2025FW賽車長袖-03.png",
-          "2025FW賽車長袖-04.png",
-          "2025FW賽車長袖-05.png",
-        ],
-        sizes: [
+          { label: "0", stock: 4 },
           { label: "1", stock: 9 },
-          { label: "2", stock: 32 },
-          { label: "3", stock: 3}
-        ]
-      }
-    ]
-  },
-  {
-    "slug": "WMS-Mohair-Knit-Short-Sweater",
-    "title": "WMS Mohair Knit Short Sweater",
-    "price": 1780,
-    "year": 2025,
-    "season": "FW",
-    "description": `
-* Short fit <br>
-* Metal logo tag <br>
-* 100% Mohair Knit<br>
-<br>
-* 短版版型<br>
-* 金屬Logo標<br>
-* 100% 馬海毛<br>
-<br>
-* Size 1<br>
-胸圍 Chest - 53cm<br>
-衣長 Length - 33cm<br>
-袖長 Sleeve - 74cm<br>
-<br>
-* Size 2<br>
-胸圍 Chest - 56cm<br>
-衣長 Length - 38cm<br>
-袖長 Sleeve - 76cm<br>
-<br>
-* 尺寸皆為商品平量數據 單位皆為公分 *<br>
-* Measurements are all in centimeters *`,
-    "variants": [
-      {
-        "color": "Blue White",
-        "colorCode": "#9ed9ee",
-        "imageFiles": [
-          "2025FW藍色馬海毛毛衣-01.png",
-          "2025FW藍色馬海毛毛衣-02.png",
-          "2025FW藍色馬海毛毛衣-03.png",
-          "2025FW藍色馬海毛毛衣-04.png"
-        ],
-        sizes: [
-          { label: "1", stock: 8 },
-          { label: "2", stock: 8 }
+          { label: "2", stock: 13 },
+          { label: "3", stock: 3 }
         ]
       }
     ]
