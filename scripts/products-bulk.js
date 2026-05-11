ce2cbd453f5f91efa1fd3ef3bdc09796fbe12fd7
TimmyJun <timmy40512@yahoo.com>
2026-05-11 22:23:12 +0800

    26SS New

--- scripts/products-bulk.js
+++ scripts/products-bulk.js
@@ -43,17 +43,86 @@ const productDefs = [
         "color": "black",
         "colorCode": "#000000",
         "imageFiles": [
-          "2026ss黑色刺繡喇叭褲-01.jpeg",
-          "2026ss黑色刺繡喇叭褲-02.jpeg",
-          "2026ss黑色刺繡喇叭褲-03.jpeg",
-          "2026ss黑色刺繡喇叭褲-04.jpeg",
-          "2026ss黑色刺繡喇叭褲-05.jpeg"
+          "2026ss黑色刺繡工裝喇叭褲-01.png",
+          "2026ss黑色刺繡工裝喇叭褲-02.png",
+          "2026ss黑色刺繡工裝喇叭褲-03.png",
+          "2026ss黑色刺繡工裝喇叭褲-04.png",
+          "2026ss黑色刺繡工裝喇叭褲-05.png",
+          "2026ss黑色刺繡工裝喇叭褲-06.png",
+          "2026ss黑色刺繡工裝喇叭褲-08.png",
+          "2026ss黑色刺繡工裝喇叭褲-09.png"
         ],
         sizes: [
-          { label: "0", stock: 4 },
+          { label: "0", stock: 3 },
           { label: "1", stock: 21 },
           { label: "2", stock: 21 },
           { label: "3", stock: 4 }
+        ]
+      }
+    ]
+  },
+  {
+    "slug": "Hole-Knit-Double-Zipper-Sweater",
+    "title": "Hole Knit Double Zipper Sweater",
+    "price": 1880,
+    "year": 2026,
+    "season": "SS",
+    "description": `
+  * Regular fit <br>
+* Double Zipper <br>
+* Hole Knit <br>
+* 100% Cotton<br>
+<br>
+* 常規版型<br>
+* 雙拉鍊<br>
+* 網洞針織<br>
+* 100% 棉材質<br>
+<br>
+* Size 1<br>
+胸圍 Chest - 63cm<br>
+衣長 Length - 54cm<br>
+袖長 Sleeve - 66cm<br>
+<br>
+* Size 2<br>
+胸圍 Chest - 67cm<br>
+衣長 Length - 60cm<br>
+袖長 Sleeve - 70cm<br>
+<br>
+* 尺寸皆為商品平量數據 單位皆為公分 *<br>
+* Measurements are all in centimeters *`,
+    "variants": [
+      {
+        "color": "Sage Green",
+        "colorCode": "#BEC5A4",
+        "imageFiles": [
+          "2026ss灰綠色洞洞拉鍊毛衣-01.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-02.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-03.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-04.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-05.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-06.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-07.png"
+        ],
+        sizes: [
+          { label: "1", stock: 10 },
+          { label: "2", stock: 10 }
+        ]
+      },
+      {
+        "color": "Iron Gray",
+        "colorCode": "#52595D",
+        "imageFiles": [
+          "2026ss鐵灰色洞洞拉鍊毛衣-01.png",
+          "2026ss鐵灰色洞洞拉鍊毛衣-02.png",
+          "2026ss鐵灰色洞洞拉鍊毛衣-03.png",
+          "2026ss鐵灰色洞洞拉鍊毛衣-04.png",
+          "2026ss鐵灰色洞洞拉鍊毛衣-05.png",
+          "2026ss鐵灰色洞洞拉鍊毛衣-06.png",
+          "2026ss灰綠色洞洞拉鍊毛衣-07.png"
+        ],
+        sizes: [
+          { label: "1", stock: 10 },
+          { label: "2", stock: 10 }
         ]
       }
     ]
